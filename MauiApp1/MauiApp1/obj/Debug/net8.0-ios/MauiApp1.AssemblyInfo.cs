fb3c4363f9c1e4bf6e7e305c18454fe3e527a7f3
letterqv <hazelirishmaearma@gmail.com>
2026-03-07 20:36:47 +0800

    committ

--- MauiApp1/MauiApp1/obj/Debug/net8.0-ios/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/MauiApp1/obj/Debug/net8.0-ios/MauiApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a6e9ae27d201fe03cfee95c0b165175e4a3f0cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee1d1322613e267e6930cb1673f236a14847578")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
