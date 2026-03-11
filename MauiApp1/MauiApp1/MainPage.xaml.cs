164a92035161ecdb7053118aedb4d52f434010b3
An <rhoangalano19@gmail.com>
2026-03-11 09:33:12 +0800

    Update MainPage.xaml.cs

--- MauiApp1/MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -1,3 +1,5 @@
+
+
 namespace MauiApp1;
 
 public partial class MainPage : ContentPage
@@ -12,5 +14,34 @@ public partial class MainPage : ContentPage
     {
         await Task.Delay(100);
         await myScroll.ScrollToAsync(0, 0, animated: true);
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Hai", "You clicked on Los Banos", "Okie");
+    }
+    private void Button2_Clicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Hai", "You clicked on Manila", "Okie");
+    }
+
+    private void Button3_Clicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Hai", "You clicked on Galleria", "Okie");
+    }
+
+    private void Button4_Clicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Hai", "You clicked on Magnolia", "Okie");
+    }
+
+    private void Button5_Clicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Hai", "You clicked on Cainta", "Okie");
+    }
+
+    private void Button6_Clicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Hai", "You clicked on Antipolo", "Okie");
     }
 }
