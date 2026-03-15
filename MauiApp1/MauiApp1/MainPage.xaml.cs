e383ed91d3e17a7be8553f58c449597077387d09
letterqv <hazelirishmaearma@gmail.com>
2026-03-15 15:50:19 +0800

    i will cry

--- MauiApp1/MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -1,11 +1,9 @@
-﻿namespace MauiApp1
-{
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-    }
+﻿namespace MauiApp1; 
 
+public partial class MainPage : ContentPage 
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 }
